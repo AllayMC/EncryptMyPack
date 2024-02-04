28f359bd689472d7cfc18679ee384d81599df444
daoge_cmd <3523206925@qq.com>
2024-02-04 14:56:28 +0800

    feat: init commit

---
+++ src/main/java/org/allaymc/EncryptMyPack.java
@@ -0,0 +1,12 @@
+package org.allaymc;
+
+/**
+ * EncryptMyPack Project 2024/2/4
+ *
+ * @author daoge_cmd
+ */
+public class EncryptMyPack {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
